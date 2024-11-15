0a7dc19531f05d4111a40bfb0a926ef78f268fb4
Ull4ash <Ullash264@hotmail.com>
2024-11-15 11:02:46 +0600

    Create salam2.js

---
+++ scripts/cmds/salam2.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "walaikumassalam",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "walaikumassalam") {
+ return message.reply({
+ body: "     「 🌸🦋 আসসালামুয়ালাইকুম\n\n\n 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\nᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/YzQuUBI.mp4")
+ });
+ }
+ }
+}
