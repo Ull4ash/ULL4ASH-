e023f0fea0a3f12aab1602f71765d4fcf78bc119
Ull4ash <Ullash264@hotmail.com>
2024-11-15 23:04:32 +0600

    Create Allahmdullah.js

---
+++ scripts/cmds/Allahmdullah.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "Allahmdullah",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "Allahmdullah") {
+ return message.reply({
+ body: "     「🫰 Allahmdullah for everything ❤️‍🩹\n\n°°°°°°\n\n \n\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n°°°°°°\n\nᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/ZEaUI9A.mp4")
+ });
+ }
+ }
+}
