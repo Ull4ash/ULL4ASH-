7455707f47797966c1efc445cef362802a8f31c4
Ull4ash <Ullash264@hotmail.com>
2024-11-15 11:12:44 +0600

    Create valobase.js

---
+++ scripts/cmds/valobase.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "valobase",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "valobase") {
+ return message.reply({
+ body: "     「 🌸🦋 \n\n\n 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\nᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/ntOlYRU.mp4")
+ });
+ }
+ }
+}
